f63f629b706d862182b264bb474ac76c022a0434
rexkenley <rexkenley@hotmail.com>
2019-12-11 15:52:53 -0500

    WIP

--- src/jsx/editorPAC.jsx
+++ src/jsx/editorPAC.jsx
@@ -103,3 +103,68 @@ export const setDescription = description => {
     console.warn(ex.message || ex);
   }
 };
+
+/**
+ * Gets the metadata of the listed entities
+ * @param  {...string} entities - list of entity names
+ * @return {Promise<MetaEntity[]>}
+ */
+export async function getMetaData(...entities) {
+  try {
+    if (!entities || !entities.length) return [];
+
+    const metas = {},
+      requests = [];
+
+    entities.forEach(e => {
+      requests.push({
+        EntityFilters: 2,
+        LogicalName: e,
+        RetrieveAsIfPublished: true,
+        getMetadata: () => ({
+          boundParameter: null,
+          operationType: 1,
+          operationName: "RetrieveEntityRequest",
+          parameterTypes: {
+            EntityFilters: {
+              typeName: "Microsoft.Dynamics.CRM.EntityFilters",
+              structuralProperty: 0
+            },
+            LogicalName: {
+              typeName: "Edm.String",
+              structuralProperty: 0
+            },
+            RetrieveAsIfPublished: {
+              typeName: "Edm.Boolean",
+              structuralProperty: 0
+            }
+          }
+        })
+      });
+    });
+
+    const result = await context.WebApi.online.executeMultiple(requests),
+      json = result && (await result.json());
+
+    /*
+      metas[e] = {
+        metadataId: get(json, "MetadataId", ""),
+        displayName: get(json, "DisplayName.LocalizedLabels[0].Label", ""),
+        entitySetName: get(json, "EntitySetName", ""),
+        attributes: get(json, "Attributes", [])
+          .filter(a => !!get(a, "DisplayName.LocalizedLabels[0].Label"))
+          .map(a => ({
+            metadataId: get(a, "MetadataId", ""),
+            displayName: get(a, "DisplayName.LocalizedLabels[0].Label", ""),
+            logicalName: get(a, "LogicalName", ""),
+            attributeType: get(a, "AttributeType", "")
+          }))
+      };
+      */
+
+    return metas;
+  } catch (e) {
+    console.error(e.message || e);
+    return null;
+  }
+}
